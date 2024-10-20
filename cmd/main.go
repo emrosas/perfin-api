e739f47eec7b13a6a4ec1ab4b90d61c5e6689dbd
Erick Mireles <emireles.rosas@gmail.com>
2024-10-20 21:58:37 +0100

    Change code to use Turso SQLite database

--- cmd/main.go
+++ cmd/main.go
@@ -1,22 +1,71 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
 	"log"
+	"os"
 
-	"github.com/emrosas/perfin-api/pkg/handlers"
-	"github.com/gin-gonic/gin"
+	// "github.com/emrosas/perfin-api/pkg/handlers"
+	// "github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
+	_ "github.com/tursodatabase/libsql-client-go/libsql"
 )
+
+type User struct {
+	Id            int
+	Username      string
+	Password      string
+	Session_token string
+	CSRF_token    string
+}
+
+func initDatabase() *sql.DB {
+	databaseURL := os.Getenv("TURSO_DATABASE_URL")
+	databaseToken := os.Getenv("TURSO_AUTH_TOKEN")
+	url := databaseURL + "?authToken=" + databaseToken
+
+	db, err := sql.Open("libsql", url)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "failed to open db %s: %s", url, err)
+		os.Exit(1)
+	}
+	fmt.Println("db opened")
+	return db
+}
+
+func queryUsers(db *sql.DB) {
+	rows, err := db.Query("SELECT * FROM users")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Query failed: %s\n", err)
+		os.Exit(1)
+	}
+	defer rows.Close()
+
+	var users []User
+
+	for rows.Next() {
+		var user User
+		if err := rows.Scan(&user.Id, &user.Username, &user.Password, &user.Session_token, &user.CSRF_token); err != nil {
+			fmt.Println("Error scanning row:", err)
+			return
+		}
+
+		users = append(users, user)
+		fmt.Println(user.Id, user.Username)
+	}
+
+	if err := rows.Err(); err != nil {
+		fmt.Println("Error during rows iteration:", err)
+	}
+}
 
 func main() {
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
-
-	r := gin.Default()
-	r.POST("/create_table", handlers.CreateTableHandler)
-	r.POST("/insert_data", handlers.InsertDataHandler)
-	r.GET("/fetch_data", handlers.FetchDataHandler)
-	r.Run() // listen and serve on 0.0.0.0:8080r := gin.Default()
+	db := initDatabase()
+	defer db.Close()
+	queryUsers(db)
 }
--- go.mod
+++ go.mod
@@ -5,10 +5,12 @@ go 1.22.2
 require github.com/jackc/pgx/v4 v4.18.3
 
 require (
+	github.com/antlr4-go/antlr/v4 v4.13.0 // indirect
 	github.com/bytedance/sonic v1.11.6 // indirect
 	github.com/bytedance/sonic/loader v0.1.1 // indirect
 	github.com/cloudwego/base64x v0.1.4 // indirect
 	github.com/cloudwego/iasm v0.2.0 // indirect
+	github.com/coder/websocket v1.8.12 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.3 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
@@ -22,9 +24,11 @@ require (
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/pelletier/go-toml/v2 v2.2.2 // indirect
+	github.com/tursodatabase/libsql-client-go v0.0.0-20240902231107-85af5b9d094d // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.2.12 // indirect
 	golang.org/x/arch v0.8.0 // indirect
+	golang.org/x/exp v0.0.0-20240325151524-a685a6edb6d8 // indirect
 	golang.org/x/net v0.25.0 // indirect
 	golang.org/x/sys v0.20.0 // indirect
 	google.golang.org/protobuf v1.34.1 // indirect
